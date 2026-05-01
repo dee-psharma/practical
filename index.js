1dd2b13d6de23fb71a117d02386ff90d04191ea4
dee-psharma <deep.sharma_cs23@gla.ac.in>
2026-05-01 13:46:30 +0530

    second commit

---
+++ index.js
@@ -0,0 +1,37 @@
+const express = require("express");
+const app = express();
+
+const PORT = 3000;
+app.use(express.json());
+
+/* -------------------------------
+   CUSTOM MIDDLEWARE (LOGGER)
+--------------------------------*/
+const logger = (req, res, next) => {
+    console.log(`${req.method} ${req.url}`);
+    next(); // move to next middleware/route
+};
+
+app.use(logger);
+
+/* -------------------------------
+   BASIC AUTH MIDDLEWARE
+--------------------------------*/
+const auth = (req, res, next) => {
+    const { username, password } = req.headers;
+
+    if (username === "admin" && password === "1234") {
+        next(); // authorized
+    } else {
+        res.status(401).json({ message: "Unauthorized" });
+    }
+};
+
+
+app.get("/", (req, res) => {
+    res.send("Server Running");
+});
+
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
